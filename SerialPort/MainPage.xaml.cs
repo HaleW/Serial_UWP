9bf087d4048cb753e9faffffb264fa115fddf077
继能 王 <hale_wang@outlook.com>
2018-09-06 20:48:02 +0800

    Add source code.

---
+++ SerialPort/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Windows.Devices.Enumeration;
+using Windows.Devices.SerialCommunication;
+using Windows.UI.Xaml.Controls;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace SerialPort
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        
+        private void ClearReceiveButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ReceiveTextBox.Text = string.Empty;
+        }
+
+        private void ClearSendButton_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            SendTextBox.Text = string.Empty;
+        }
+    }
+}
